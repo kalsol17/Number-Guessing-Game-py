c1348c25db160aaefde82342839ee11afdeb532d
kaleab solomon <kaleab24gebreselassie@gmail.com>
2025-08-20 23:02:13 +0300

    ☀ initial commit

---
+++ SimpleInterestCalculator.py
@@ -0,0 +1,10 @@
+principal=input("Enter the principal amount: ")
+rate=input("Enter the rate of the interest: ")
+time=input('Enter the time in years: ')
+
+principal =float(principal)
+rate=float(rate)
+time==float(time)
+
+simple_interest=(principal * rate * time)/100
+print("The simple Interest is :", simple_interest)
